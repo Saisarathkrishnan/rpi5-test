2e0ef0ea9edb9aac7e56c52935f6c05624176bfb
mrm <mail@example.com>
2025-08-16 14:06:15 +0100

    damn it works

--- testSai.py
+++ testSai.py
@@ -1,35 +1,26 @@
-# camera_stream.py
-import json
-from picamera2 import Picamera2
 from flask import Flask, Response
+from picamera2 import Picamera2
 import cv2
-import os
-import time
 
-app=Flask(__name__)
-# Load JSON tuning file
-with open("/home/mrm/xxx.json") as f:
-    controls_json = json.load(f)
+app = Flask(__name__)
 
+tuning_data = Picamera2.load_tuning_file("/home/mrm/xxx.json")
+# Initialize camera without custom tuning file
+picam2 = Picamera2(tuning=tuning_data)
+# Comment out custom tuning file to test default
 
-picam2 = Picamera2()
-if "controls" in controls_json:
-    for key, value in controls_json["controls"].items():
-        picam2.set_controls({key: value})
-config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (640, 480)})
+config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (1000, 1000)})
 picam2.configure(config)
 picam2.start()
+print("Camera started with default tuning!")
 
-print("Camera started with tuning!")
-
-time.sleep(5)
 def generate_frames():
     while True:
         frame = picam2.capture_array()
+#        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         ret, buffer = cv2.imencode('.jpg', frame)
         if not ret:
             continue
-
         frame_bytes = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
